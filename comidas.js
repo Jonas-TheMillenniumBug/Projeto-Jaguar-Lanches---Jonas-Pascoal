bad416090a507402092a9af19e456a147dbf2d1e
Jonas-TheMillenniumBug <jonas.neto06@aluno.ifce.edu.br>
2024-08-15 21:19:27 -0300

    Add files via upload

---
+++ comidas.js
@@ -0,0 +1,50 @@
+var combos = [
+  {
+    id: 1,
+    url: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/64560156-be4a-4cb3-b134-e01c1ed34d34/202201142013_Q5CJ_i.jpg",
+    descricao: "3 Pizzas Promocionais + 1 Guaraná 1.5 L",
+    preco: 91.99,
+  },
+  {
+    id: 2,
+    url: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/64560156-be4a-4cb3-b134-e01c1ed34d34/202307112136_3M1M_i.jpg",
+    descricao: "4 Pip Boys + 1 Batata Fallout + 1 Nuka Cola 1.5 L.",
+    preco: 77.43,
+  },
+  {
+    id: 3,
+    url: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/64560156-be4a-4cb3-b134-e01c1ed34d34/202204031801_JICK_i.jpg",
+    descricao: "3 Pizzas Promocionais + 1 Sukita 1.5 L.",
+    preco: 91.99,
+  },
+  {
+    id: 4,
+    url: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/64560156-be4a-4cb3-b134-e01c1ed34d34/202203061709_3WB0_i.jpg",
+    descricao: "2 Bombas Atômicas + 1 Batata Russia + 1 Pearl Harbor 1.5 L.",
+    preco: 89.99,
+  },
+  {
+    id: 5,
+    url: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/64560156-be4a-4cb3-b134-e01c1ed34d34/202209011219_5FQO_i.jpg",
+    descricao: " Frango/Bacon com Catupiry + Borda de cheedar + 1 Coca Cola 1.5 L.",
+    preco: 62.99,
+  },
+  {
+    id: 6,
+    url: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/64560156-be4a-4cb3-b134-e01c1ed34d34/202209011213_8QLD_i.jpg",
+    descricao: "Portuguesa com borda Catupiry + 1  Guarana 1.5 L.",
+    preco: 72.99,
+  },
+  {
+    id: 7,
+    url: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/64560156-be4a-4cb3-b134-e01c1ed34d34/202307112020_AEUY_i.jpg",
+    descricao: "4 X-Tudo De Frango + 1 Refri 1.5 L.",
+    preco: 48.99,
+  },
+  {
+    id: 8,
+    url: "https://static.ifood-static.com.br/image/upload/t_medium/pratos/64560156-be4a-4cb3-b134-e01c1ed34d34/202106251626_40S6_i.jpg",
+    descricao: "Combo familia pizza para comer e ser feliz",
+    preco: 109.99,
+  },
+];
